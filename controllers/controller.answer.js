48d1a4bdc1a7aafe86a112bd6b68bea182aca5c4
NewDivide41194 <heinhtet25944@gmail.com>
2021-02-04 15:15:43 +0630

    updateQuestionAPI

--- controllers/controller.answer.js
+++ controllers/controller.answer.js
@@ -10,10 +10,10 @@ const addAnswer = (req, res) => {
     let targetCount = req.body.data.length;
 
     let count = 0;
-    let queryLoop = new Promise((resolve, reject) => { // console.log("req.body is", req.body.data[0])
+    let queryLoop = new Promise((resolve, reject) => { 
+        console.log("req.body is", req.body.data[0])
         answerService.deleteAnswer(req.body.data[0].userId, req.body.data[0].survey_headers_id, req.body.data[0].building_id);
         req.body.data.map(async (data) => {
-            console.log("data.file is", data);
             let optionChoiceId = data.optionChoiceId;
             let other = data.other;
             let userId = data.userId;
